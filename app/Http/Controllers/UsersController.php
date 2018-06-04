0f640eca03ab9d6ebc6995dda771902d768c8415
Sanjampreet Singh <sanjam531@gmail.com>
2018-06-04 23:11:04 +0530

    updated usersController added more if {} and updated env with mongodb

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Auth;
 use Illuminate\Http\Request;
-use Log;
 use Session;
 use Validator;
 
@@ -14,13 +13,11 @@ class UsersController extends Controller
 
     /**
      * Post Login request
-     *
      * @param Request $request
      * @return authentication and login view
      */
     public function store(Request $request)
     {
-
         // validate the info, create rules for the inputs
         $rules = array(
             'username' => 'required|min:3', // make sure the username
@@ -37,49 +34,52 @@ class UsersController extends Controller
                 ->withInput(Input::except('password')); // send back the input (not the password) so that we can repopulate the form
         } else {
             //log the user in
-            //add is_active in post method
-            //'is_active' => '1'
             $credentials = $request->only(['username', 'password']);
-            /**
+            /*
              * Attempting Authentication
              * Else Redirect Back
              */
             if (Auth::attempt($credentials)) {
-                /**
-                 * Switch Case for USER Type
+                /*
+                 * Checking status of user
                  */
-                switch ($user = Auth::user()->type) {
-                    case 'MASTER_ADMIN':
-                        Session::flash('msg', 'You have been logged in');
-                        return "1";
-                        break;
-                    case 'EXECUTIVE_MEMBER':
-                        Session::flash('msg', 'You have been logged in');
-                        return "2";
-                        break;
-                    case 'STUDENT':
-                        Session::flash('msg', 'You have been logged in');
-                        return "3";
-                        break;
-                    case 'COMPANY':
-                        Session::flash('msg', 'You have been logged in');
-                        return "4";
-                        break;
-                    default:
-                        return redirect()->back();
-                        break;
+                if (Auth::user()->is_verified == 1 && Auth::user()->is_active == 1) {
+                    /*
+                     * Switch Case for USER Type
+                     */
+                    switch ($user = Auth::user()->type) {
+                        case 'MASTER_ADMIN':
+                            Session::flash('msg', 'You have been logged in');
+                            return "1";
+                            break;
+                        case 'EXECUTIVE_MEMBER':
+                            Session::flash('msg', 'You have been logged in');
+                            return "2";
+                            break;
+                        case 'STUDENT':
+                            Session::flash('msg', 'You have been logged in');
+                            return "3";
+                            break;
+                        case 'COMPANY':
+                            Session::flash('msg', 'You have been logged in');
+                            return "4";
+                            break;
+                        default:
+                            return redirect()->back();
+                            break;
+                    }
+                } else {
+                    return redirect()->back();
                 }
             } else {
                 return redirect()->back();
             }
         }
     }
-
     // app/controllers/HomeController.php
     public function doLogout()
     {
         Auth::logout(); // log the user out of our application
         return Redirect::to('auth/login'); // redirect the user to the login screen
     }
-
 }
