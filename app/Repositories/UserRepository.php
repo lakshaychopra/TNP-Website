2fc7c75b7aa402b7a71622f15586115bfd4c7f85
Sanjampreet Singh <sanjam531@gmail.com>
2018-06-08 02:03:10 +0530

    Created Repos. and .env file configured for mailing

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+
+class UserRepository
+{
+    /**
+     * create new instance
+     *
+     * @param User $model
+     */
+    public function __construct(User $model)
+    {
+        $this->model = $model;         
+    }
+    
+    
+}
