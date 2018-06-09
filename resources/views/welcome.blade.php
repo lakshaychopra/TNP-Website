6544781e275c45d6369cde56c3d81d3f7ddc2734
Sanjampreet Singh <sanjam531@gmail.com>
2018-06-09 14:22:54 +0530

    welcome blade updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,36 +1,11 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-<<<<<<< HEAD
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        {{-- <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css"> --}}
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/fontawesome.min.css') }}">
-
-        <!-- Styles -->
-
-    </head>
-    <body>
-
-            <div class="content" id="app">
-                <router-view></router-view>
-            </div>
-    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
-=======
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Laravel</title>
->>>>>>> f7606e2a7675835c7462e33ed70421f171d88ba2
     
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
