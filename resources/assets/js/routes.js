8bea2b5c22d7f0edb2377d235fe8a95af48da4c1
Sanjampreet Singh <sanjam531@gmail.com>
2018-06-11 16:57:11 +0530

    VUE RESOURCE ADDED ROUTE.JS

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import VueResource from 'vue-resource'
 Vue.use(VueRouter)
-// Vue.use(VueResource)
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
 const router = new VueRouter({ 
     base: '/',
     mode: 'history',
