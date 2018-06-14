5c5923da5a449cdcebc2a0ce8f7b7b795590e3aa
Sanjampreet Singh <sanjam531@gmail.com>
2018-06-14 20:14:42 +0530

    extra features removed from navbar and zh lang removed

--- resources/assets/js/pages/layout/components/Navbar.vue
+++ resources/assets/js/pages/layout/components/Navbar.vue
@@ -7,7 +7,7 @@
     <div class="right-menu">
       <error-log class="errLog-container right-menu-item"></error-log>
 
-      <el-tooltip effect="dark" :content="$t('navbar.screenfull')" placement="bottom">
+      <!-- <el-tooltip effect="dark" :content="$t('navbar.screenfull')" placement="bottom">
         <screenfull class="screenfull right-menu-item"></screenfull>
       </el-tooltip>
 
@@ -15,11 +15,12 @@
 
       <el-tooltip effect="dark" :content="$t('navbar.theme')" placement="bottom">
         <theme-picker class="theme-switch right-menu-item"></theme-picker>
-      </el-tooltip>
+      </el-tooltip> -->
 
       <el-dropdown class="avatar-container right-menu-item" trigger="click">
         <div class="avatar-wrapper">
-          <img class="user-avatar" :src="avatar+'?imageView2/1/w/80/h/80'">
+          <img class="user-avatar">
+           <!-- :src="avatar+'?imageView2/1/w/80/h/80'">  image added here --> 
           <i class="el-icon-caret-bottom"></i>
         </div>
         <el-dropdown-menu slot="dropdown">
@@ -28,11 +29,11 @@
               {{$t('navbar.dashboard')}}
             </el-dropdown-item>
           </router-link>
-          <a target='_blank' href="https://github.com/PanJiaChen/vue-element-admin/">
-            <el-dropdown-item>
-              {{$t('navbar.github')}}
-            </el-dropdown-item>
-          </a>
+          <!-- <a target='_blank' href="https://github.com/PanJiaChen/vue-element-admin/"> -->
+            <!-- <el-dropdown-item> -->
+              <!-- {{$t('navbar.github')}} -->
+            <!-- </el-dropdown-item> -->
+          <!-- </a> -->
           <el-dropdown-item divided>
             <span @click="logout" style="display:block;">{{$t('navbar.logOut')}}</span>
           </el-dropdown-item>
@@ -43,22 +44,22 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from 'vuex'     
 import Breadcrumb from '../../../components/Breadcrumb'
 import Hamburger from '../../../components/Hamburger'
 import ErrorLog from '../../../components/ErrorLog'
-import Screenfull from '../../../components/Screenfull'
-import LangSelect from '../../../components/LangSelect'
-import ThemePicker from '../../../components/ThemePicker'
+// import Screenfull from '../../../components/Screenfull'
+// import LangSelect from '../../../components/LangSelect'
+// import ThemePicker from '../../../components/ThemePicker'
 
 export default {
   components: {
     Breadcrumb,
     Hamburger,
     ErrorLog,
-    Screenfull,
-    LangSelect,
-    ThemePicker
+    // Screenfull,
+    // LangSelect,
+    // ThemePicker
   },
   computed: {
     ...mapGetters([
@@ -108,15 +109,15 @@ export default {
       display: inline-block;
       margin: 0 8px;
     }
-    .screenfull {
-      height: 20px;
-    }
-    .international{
-      vertical-align: top;
-    }
-    .theme-switch {
-      vertical-align: 15px;
-    }
+    // .screenfull {
+    //   height: 20px;
+    // }
+    // .international{
+    //   vertical-align: top;
+    // }
+    // .theme-switch {
+    //   vertical-align: 15px;
+    // }
     .avatar-container {
       height: 50px;
       margin-right: 30px;
