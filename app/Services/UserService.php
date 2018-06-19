ad12f0e7b0968a1feeaaf272a1336e14f2e9a4bd
Sanjampreet Singh <sanjam531@gmail.com>
2018-06-19 23:15:13 +0530

    user Services updated

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -10,5 +10,24 @@ class UserService
 	public function __construct(UserRepository $respository)
 	{
 		$this->respository = $respository;
-    }
+	}
+	
+	public function listUser(){
+		return $this->repository->list();
+	}
+	
+	public function createUser(array $payload)
+	{
+		return $this->repository->create($payload);
+	}
+	
+	public function updateUser(array $payload, $id)
+	{
+		return $this->repository->update($payload);
+	}
+
+	public function deleteUser(array $payload, $id)
+	{
+		return $this->repository->delete($payload);
+	}
 }
