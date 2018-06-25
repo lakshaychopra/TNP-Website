c5cbbf0814f24fffb20957069da800cc4de73058
Sanjampreet Singh <sanjam531@gmail.com>
2018-06-25 22:35:12 +0530

    pinned added to post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,7 +9,11 @@ class Post extends Model
     protected $table = 'posts';
 
     protected $fillable = [
-        'title', 'body','user_id','username', 'tag', 'category','post_link','image'
+        'title','body','user_id','username', 'tag', 'category','post_link','image'
     ];
     
+    public function isPinned(){
+        return $this->is_pinned;
+    }
+
 }
