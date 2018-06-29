3a50ad6ed439096e7b61bb3a099b7f16dde8db7d
Sanjampreet Singh <sanjam531@gmail.com>
2018-06-29 12:34:24 +0530

    student DB added

--- database/migrations/2018_06_25_145915_create_students_table.php
+++ database/migrations/2018_06_25_145915_create_students_table.php
@@ -7,23 +7,49 @@ use Illuminate\Database\Migrations\Migration;
 class CreateStudentsTable extends Migration
 {
     /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+    * Run the migrations.
+    *
+    * @return void
+    */
     public function up()
     {
         Schema::create('students', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('university_roll_no')->unique();
+            $table->string('class_roll_no')->unique()->comment('6 digit numeric unique')->nullable();
+            $table->string('name');
+            $table->string('batch')->comment('Year of Addmission and Year of Passout')->nullable();
+            $table->enum('branch_type',['B.Tech., M.B.A., M.C.A., M.Tech.'])->nullable();
+            $table->string('stream')->comment('Stream/Course type')->nullable();
+            $table->enum('training_semester',['7','8'])->nullable();
+            $table->enum('shift',['MORNING, EVENING'])->nullable();
+            $table->string('section')->comment('Class Section')->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->string('phone_number')->unique()->nullable();
+            $table->enum('gender',['MALE,FEMALE'])->nullable();
+            $table->enum('category',['OBC, SC, ST, GEN'])->nullable();
+            $table->string('blood_group')->nullable();
+            $table->integer('height')->comment('in cm')->nullable();
+            $table->integer('weight')->comment('in kg')->nullable();
+            $table->string('father_name')->nullable();
+            $table->integer('father_phone_number')->nullable();
+            $table->string('mother_name')->nullable();
+            $table->integer('mother_phone_number')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('district')->nullable();
+            $table->integer('pincode')->nullable();
+            $table->enum('form_status',['NONE','PENDING','RECIEVED','SUBMITTED'])->default('NONE')->nullable();
             $table->timestamps();
         });
     }
-
+    
     /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+    * Reverse the migrations.
+    *
+    * @return void
+    */
     public function down()
     {
         Schema::dropIfExists('students');
