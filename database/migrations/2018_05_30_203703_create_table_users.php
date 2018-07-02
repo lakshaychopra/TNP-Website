d574066951727a2cd9b08ea14f21b2af77a9fe22
Sanjampreet Singh <sanjam531@gmail.com>
2018-07-02 13:59:05 +0530

    some changes in user table

--- database/migrations/2018_05_30_203703_create_table_users.php
+++ database/migrations/2018_05_30_203703_create_table_users.php
@@ -15,10 +15,10 @@ class CreateTableUsers extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username');
+            $table->string('username')->unique();
             $table->string('password');
-            $table->string('email');
-            $table->string('phone_number');
+            $table->string('email')->unique();
+            $table->string('phone_number')->unique();
             $table->string('token_2fa')->nullable();
             $table->enum('type', ['STUDENT', 'COMPANY', 'EXECUTIVE_MEMBER', 'MASTER_ADMIN'])->default('STUDENT');
             $table->boolean('is_active')->default(false);
