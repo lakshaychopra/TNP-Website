383e7f658f7bf1323fc76f1f95225c9d7b13eda1
Sanjampreet Singh <sanjam531@gmail.com>
2018-07-16 23:16:04 +0530

    some changes

--- app/Events/UserSingleCreateEvent.php
+++ app/Events/UserSingleCreateEvent.php
@@ -22,7 +22,7 @@ class UserSingleCreateEvent
      *
      * @return void
      */
-    public function __construct(User $user)
+    public function __construct($user)
     {
         $this->user = $user;
     }
