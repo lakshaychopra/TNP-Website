926ba22a69479a97718670dfc8824931726f0af3
Sanjampreet Singh <sanjam531@gmail.com>
2018-07-17 12:11:17 +0530

    migerations

---
+++ database/migrations/2018_07_17_062726_create_previous_marks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePreviousMarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('previous_education', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('univ_roll_no')->nullable();
+            $table->string('board')->nullable();
+            $table->string('month')->nullable();
+            $table->integer('year')->nullable();
+            $table->integer('obtained_marks')->nullable();
+            $table->integer('max_marks')->nullable();
+            $table->string('institute')->nullable();
+            $table->string('year_gap')->nullable();            
+            $table->string('jee_rank')->nullable();
+            $table->enum('education',['X','XII','DIPLOMA']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('previous_education');
+    }
+}
