762742f92e310f59402c9cbaa4a9e79b46645313
Sanjampreet Singh <sanjam531@gmail.com>
2018-07-17 23:36:38 +0530

    migeration added

---
+++ database/migrations/2018_07_17_171528_create_student_semester_marks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentSemesterMarksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('semester_marks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('univ_roll_no')->nullable();
+            $table->integer('semester')->nullable();
+            $table->integer('marks_obtained')->nullable();
+            $table->integer('sgpa')->nullable();
+            $table->integer('credits')->nullable();
+            $table->integer('active_backlog')->nullable();
+            $table->integer('passive_backlog')->nullable();
+            $table->enum('marks_type',['SGPA','MARKS'])->default('SGPA');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('semester_marks');
+    }
+}
