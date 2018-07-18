3250de923d9d7de3a258c0e57c7607cdf6d58260
Karamveer Singh <kmvsingh007@gmail.com>
2018-07-18 19:02:22 +0530

    index page

--- resources/assets/js/layouts/index/pages.vue
+++ resources/assets/js/layouts/index/pages.vue
@@ -223,14 +223,35 @@
                             <div class="row">
                                 <div class="col-md-12">
                                         <div class="card">
-                                            <div class="card-header">
-                                        <h4>  Contact</h4>
+                                            <div class="card-header" id="office">
+                                        <h4>T&P OFFICE</h4>
                                             </div>
+
                                             <div class="card-body">
-                                                <ul>
-                                                    <li><a href="#">Placement</a></li>
-                                                    <li><a href="#">Internship</a></li>
-                                                </ul>
+                                                <div class="office text-center">
+                                                    <img src="/images/sodhi.jpg" width="60%" alt="">
+                                                </div>
+                                              <div class="row">
+                                                  <div class="col-md-12">
+                                                      <p class="mb-0"><strong>Dr. G. S. SODHI</strong></p>
+                                                  </div> 
+                                              </div>
+                                              <div class="row">
+                                                  <div class="col-md-12">
+                                                      <small>Officer (Training & Placement)</small>
+                                                  </div> 
+                                              </div>
+                                              <div class="row">
+                                                  <div class="col-md-12">
+                                                      <small>E-mail: gssodhi@gndec.ac.in</small>
+                                                  </div> 
+                                              </div>  
+                                            <div class="row">
+                                                  <div class="col-md-12">
+                                                      <small>Cell: 9999999999</small>
+                                                  </div> 
+                                              </div>  
+                                                
                                             </div>
                                         </div>
                                 </div>
@@ -238,14 +259,38 @@
                             <div class="row">
                                 <div class="col-md-12">
                                         <div class="card">
-                                            <div class="card-header">
-                                        <h4>  Category</h4>
+                                            <div class="card-header" id="about">
+                                        <h4>  ABOUT</h4>
                                             </div>
-                                            <div class="card-body">
-                                                <ul>
-                                                    <li><a href="#">Placement</a></li>
-                                                    <li><a href="#">Internship</a></li>
-                                                </ul>
+                                            <div class="card-map">
+                                                <a href="https://wego.here.com/directions/mix/mylocation/e-eyJuYW1lIjoiR25kZWMtIFRyYWluaW5nIGFuZCBQbGFjZW1lbnQiLCJhZGRyZXNzIjoiR3VydSBOYW5hayBEZXYgRW5naW5lZXJpbmcgQ29sbGVnZSBHaWxsIFBhcmsgTHVkaGlhbmEgMTQxMDA2LCBMdWRoaWFuYSwgUHVuamFiLCBJbmRpYSAxNDEwMDYiLCJsYXRpdHVkZSI6MzAuODU5MzM3NTA3ODAzLCJsb25naXR1ZGUiOjc1Ljg2MTY3Mjk3MzYzMywicHJvdmlkZXJOYW1lIjoiZmFjZWJvb2siLCJwcm92aWRlcklkIjoyODg1Nzc3NzExOTM4ODB9?map=30.859337507803,75.861672973633,15,normal&fb_locale=en_GB" rel="dialog" target="_blank" role="button" class="" tabindex="0"><div class="_4j7v _5bld"><img class="_a3f img" alt="" aria-label="Map attachment" src="https://external.fluh1-1.fna.fbcdn.net/static_map.php?region=IN&amp;v=42&amp;osm_provider=2&amp;size=306x98&amp;zoom=15&amp;markers=30.85933751%2C75.86167297&amp;language=en_GB" width="306" height="98"></div></a>
+                                            </div>
+                                            <div class="card-body" style="padding-top:20px;">
+                                                <!-- <iframe style="pointer-events:none; border:0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3424.9278353122977!2d75.85737781489665!3d30.860695381592787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391a828f09011b15%3A0xbf3f5b51dcc81b12!2sGuru+Nanak+Dev+Engineering+College!5e0!3m2!1sen!2sin!4v1531911243199" width="100%" height="200" frameborder="0"></iframe> -->
+                                            <div class="row">
+                                                <div class="col-md-2">
+                                                    <i class="fa fa-compass mt-1"  aria-hidden="true"></i>
+                                                </div>
+                                                <div class="col-md-10">
+                                                    <small>Guru Nanak Dev Engineering College Gill Park Ludhiana 141006 (7.11 km) 141006 Ludhiana, Punjab, India <br> <a href="https://wego.here.com/directions/mix/mylocation/e-eyJuYW1lIjoiR25kZWMtIFRyYWluaW5nIGFuZCBQbGFjZW1lbnQiLCJhZGRyZXNzIjoiR3VydSBOYW5hayBEZXYgRW5naW5lZXJpbmcgQ29sbGVnZSBHaWxsIFBhcmsgTHVkaGlhbmEgMTQxMDA2LCBMdWRoaWFuYSwgUHVuamFiLCBJbmRpYSAxNDEwMDYiLCJsYXRpdHVkZSI6MzAuODU5MzM3NTA3ODAzLCJsb25naXR1ZGUiOjc1Ljg2MTY3Mjk3MzYzMywicHJvdmlkZXJOYW1lIjoiZmFjZWJvb2siLCJwcm92aWRlcklkIjoyODg1Nzc3NzExOTM4ODB9?map=30.859337507803,75.861672973633,15,normal&fb_locale=en_GB" target="_blank">Get Directions</a></small>
+                                                </div>
+                                            </div>
+                                            <div class="row mt-2">
+                                                <div class="col-md-2">
+                                                    <i class="fa fa-phone mt-1"  aria-hidden="true"></i>
+                                                </div>
+                                                <div class="col-md-10">
+                                                    <small>099157 81209</small>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-2">
+                                                    <i class="fa fa-globe mt-1"  aria-hidden="true"></i>
+                                                </div>
+                                                <div class="col-md-10">
+                                                    <small><a href="https://www.gndec.ac.in" target="_blank">www.gndec.ac.in</a></small>
+                                                </div>
+                                            </div>
                                             </div>
                                         </div>
                                 </div>
@@ -258,6 +303,8 @@
 </div>
 </template>
 <script>
+
+
     // import AppHeader from './header.vue'
     import { addHomePostURL } from "../../config.js";
     import { searchURL } from "../../config.js";
@@ -465,8 +512,13 @@
     bottom: -2px;
     left: 0;
 }
+#sidebar #about h4:after{
+        content: "\f0c0";
+}
+#sidebar #office h4:after{
+        content: "\f1ad";
+}
 #sidebar .card-header h4:after{
-    content: "\f0b0";
     display: block;
     float: left;
     background-color: #2f3c4e;
