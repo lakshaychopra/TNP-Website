a474f9f40cc1726d29efd6641ccc9112f360076a
Sanjampreet Singh <sanjam531@gmail.com>
2018-07-26 22:41:13 +0530

    tempo

--- resources/assets/js/config.js
+++ resources/assets/js/config.js
@@ -7,20 +7,20 @@ export const securityURL = '/api/security';
 export const oauthURL = '/oauth/token';
 
 export const addPostURL = '/api/dashboard/post';
-export const showPostURL = '/api/dashboard/post';
+export const showPostURL = '/api/dashboard/post/';
 
-export const addHomePostURL = '/api/home/';
-export const pinnedPostURL = '/api/home/page/view/pinned';
-export const registerURL = '/api/register/';
+export const addHomePostURL = '/api/home';
+export const pinnedPostURL = '/api/home/page/view/pinned/';
+export const registerURL = '/api/register';
 export const setPasswordURL = '/api/register/password';
 
 export const downloadExcelURL = '/api/dashboard/user/excelfile';
 export const mailExcelURL = '/api/dashboard/user/createmail';
 export const singleuserURL = '/api/dashboard/user/form/create';
 
-export const searchURL = '/api/home/post/search';
-export const categoryURL = '/api/home/post/view';
-export const rightWidget = '/api/home/page/view/widgets';
+export const searchURL = '/api/home/post/search/';
+export const categoryURL = '/api/home/post/view/';
+export const rightWidget = '/api/home/page/view/widgets/';
 
 export const formstepChangeURL = '/api/dashboard/user/step/update';
 
@@ -30,4 +30,4 @@ export const storeStudentURL = '/api/dashboard/student';
 
 export const updateProfileURL = 'api/dashboard/student';
 
-export const viewPost = '/view';
+export const viewPost = '/view/';
