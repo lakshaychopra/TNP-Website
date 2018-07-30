c69b2a15de4aac0cfddab6f04d88d36bf93261ac
Sanjampreet Singh <sanjam531@gmail.com>
2018-07-30 12:49:50 +0530

    env 2

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@tnpgndec.com'),
+        'name' => env('MAIL_FROM_NAME', 'T&P, GNDEC'),
     ],
 
     /*
