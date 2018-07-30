09a36f2abe3454df4d0b861d3d687baf6aac4462
Sanjampreet Singh <sanjam531@gmail.com>
2018-07-30 19:45:54 +0530

    kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,10 +24,7 @@ class Kernel extends ConsoleKernel
     */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('queue:work --tries=3')
-        ->everyMinute()
-        ->withoutOverlapping();
-        
+        $schedule->command('queue:work queue:work --daemon --tries=3')->everyMinute()->withoutOverlapping();
     }
     
     /**
