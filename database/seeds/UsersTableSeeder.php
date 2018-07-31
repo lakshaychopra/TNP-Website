15b95f3753ea20974ca6cabe060ca0f67693c765
Sanjampreet Singh <sanjam531@gmail.com>
2018-07-31 12:34:16 +0530

    seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,10 +13,10 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $user = new User;
-        $user->username ="123456789012";        
+        $user->username ="DevChannel";        
         $user->password=bcrypt("12346789");
-        $user->email="sanjam531@gmail.com";
-        $user->phone_number="9815333040";
+        $user->email="kasam199796@gmail.com";
+        $user->phone_number="1234567890";
         $user->type = "EXECUTIVE_MEMBER";
         $user->is_mailed="1";
         $user->is_active="1";
