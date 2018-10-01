a35c1968417bb18a4193feb4c2fe0a72488ebad7
Sanjampreet Singh <sanjam531@gmail.com>
2018-10-01 21:22:20 +0530

    links updated

--- resources/assets/js/layouts/index/show.vue
+++ resources/assets/js/layouts/index/show.vue
@@ -160,7 +160,7 @@
          <footer class="container-fluid footer mt-5 " style="left:0">
             <p class="text-center"><small>Developed with <span class="text-danger"><i class="fa fa-heart" aria-hidden="true"></i>
                     </span> by
-                    <a href="/humans.txt">Genconians</a> | © 2018 <a href="https://www.gndec.ac.in/" target="_blank">GNDEC,
+                    <a href="/genconian">Genconians</a> | © 2018 <a href="https://www.gndec.ac.in/" target="_blank">GNDEC,
                         Ldh</a></small></p>
         </footer>
     </div>
