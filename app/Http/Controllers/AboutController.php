4c7fa0a160b290bc497fb02c2560fb85eaf75aab
Sanjampreet Singh <sanjam531@gmail.com>
2018-10-04 09:18:19 +0530

    About Controller

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -26,9 +26,12 @@ class AboutController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $auth = JWTAuth::parseToken()->authenticate();
+        $limit  = $request->input('limit') ?? 10;
+        $posts = $this->repository->list($limit);
+        return $this->respondData($posts);
     }
 
     /**
@@ -47,9 +50,27 @@ class AboutController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreateAboutRequest $request)
     {
-        //
+        $auth = JWTAuth::parseToken()->authenticate();
+        try {
+            DB::beginTransaction();
+            if (!$auth) {
+                return $this->respondUnauthorized('About Failed');
+            }
+            $about = $request->all();
+            if ($request->hasFile('image')) {
+                $about['image'] = $this->service->uploadAboutImageService($about);
+            } 
+            
+            $aboutCreate = $this->service->createAbout($about);
+            DB::commit();
+            return $this->respondSuccess('Inserted', $aboutCreate);
+        }
+        catch (Exception $e) {
+            DB::rollback();
+            return $this->respondException($e);
+        }
     }
 
     /**
@@ -58,9 +79,10 @@ class AboutController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(About $about)
     {
-        //
+        $auth = JWTAuth::parseToken()->authenticate();
+        return $this->respondData($about);
     }
 
     /**
@@ -69,9 +91,10 @@ class AboutController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(About $about)
     {
-        //
+        $auth = JWTAuth::parseToken()->authenticate();
+        return $this->respondData($about);
     }
 
     /**
@@ -81,19 +104,44 @@ class AboutController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(CreateAbouttRequest $request,About $about)
     {
-        //
+        $auth = JWTAuth::parseToken()->authenticate();
+        try {
+            DB::beginTransaction();
+            if(!$auth){
+                return $this->respondError('Failed', 401); 
+            }
+            $data = $request->all();
+            $about->title = $request->title;
+            $about->body = $request->body;
+            $about->tag = $request->tag;
+            $about->category = $request->category;
+            if ($request->hasFile('image')) {
+                $about['image'] = $this->service->uploadAboutImageService($data);
+            } 
+            $about->save(); 
+            DB::commit();
+            return $this->respondSuccess('Updated',$about);
+        } catch (Exception $e) {
+            DB::rollback();
+            return $this->respondException($e);
+        }
     }
-
+    
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(About $about)
     {
-        //
+        $auth = JWTAuth::parseToken()->authenticate();
+        // $post['image'] = $this->service->deletePostImage($post);
+        // $delete = Post::destroy($post->id);
+        $delete = $this->repository->delete($about);
+        $index= About::orderBy('created_at', 'desc')->get();
+        return $this->respondSuccess('Deleted', $index);
     }
 }
