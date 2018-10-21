397cab9be437711c9170f999440b52201d115c9a
Sanjampreet Singh <sanjam531@gmail.com>
2018-10-21 17:41:52 +0530

    semester marks db

--- database/migrations/2018_07_17_171528_create_student_semester_marks_table.php
+++ database/migrations/2018_07_17_171528_create_student_semester_marks_table.php
@@ -17,13 +17,15 @@ class CreateStudentSemesterMarksTable extends Migration
             $table->increments('id');
             $table->string('univ_roll_no')->unique();
             $table->string('semester')->nullable();
-            $table->string('marks_obtained')->nullable();
-            $table->string('sgpa')->nullable();
+            $table->string('obtained_marks')->nullable();
+            $table->string('max_marks')->nullable();
             $table->string('credits')->nullable();
             $table->string('active_backlog')->nullable();
             $table->string('passive_backlog')->nullable();
             $table->string('stream')->comment('Stream/Course type')->nullable();
-            $table->enum('marks_type',['SGPA','MARKS'])->default('SGPA');
+            $table->enum('marks_type',['CGPA','PERCENTAGE'])->default('CGPA');
+            $table->string('percentage')->nullable();
+            $table->string('semester_status')->nullable();
             $table->timestamps();
         });
     }
