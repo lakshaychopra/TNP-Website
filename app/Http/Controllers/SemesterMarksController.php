28473c255afed2f0f1eb9b14f7da1814910efe21
Sanjampreet Singh <sanjam531@gmail.com>
2018-10-21 17:51:28 +0530

    Semester Requests, Model and Controller

---
+++ app/Http/Controllers/SemesterMarksController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SemesterMarks;
+use Illuminate\Http\Request;
+
+class SemesterMarksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SemesterMarks  $semesterMarks
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SemesterMarks $semesterMarks)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SemesterMarks  $semesterMarks
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SemesterMarks $semesterMarks)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SemesterMarks  $semesterMarks
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SemesterMarks $semesterMarks)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SemesterMarks  $semesterMarks
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SemesterMarks $semesterMarks)
+    {
+        //
+    }
+}
